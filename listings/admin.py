527f7f3f89a266ba1b56b05091d9222d3b00a8f4
Pankaj Prasun <pankaj1.prasun@ril.com>
2021-08-30 17:36:44 +0530

    new migration admin setting

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Listing
+admin.site.register(Listing)
 # Register your models here.
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Realtor
+
+admin.site.register(Realtor)
 
 # Register your models here.
