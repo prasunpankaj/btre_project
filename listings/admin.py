112cbeb3e512093cbd1013f9d2aeed12716d775c
Pankaj Prasun <pankaj1.prasun@ril.com>
2021-09-04 00:18:35 +0530

    Admin Area Changes

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Listing
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_per_page = 25 # No of records per page 
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor','price',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+admin.site.register(Listing, ListingAdmin)
 # Register your models here.
