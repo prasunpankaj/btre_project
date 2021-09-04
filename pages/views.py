559a4f0f9e55d6dd95ec0b21de6ef8b4efddb709
Pankaj Prasun <pankaj1.prasun@ril.com>
2021-09-04 22:39:30 +0530

    Seller of Month and Team Member

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 # Create your views here.
 
 def index(request):
     #return HttpResponse('<h1>Hello World</h>')
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(reqest):
-    return render(reqest, 'pages/about.html')
+    #Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    #Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(reqest, 'pages/about.html', context)
 
 def tnc(request):
     return render(request, 'pages/tnc.html')
