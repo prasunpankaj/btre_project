4d52e2d7da497ed33ac4684dc8d1db81092549c2
Pankaj Prasun <pankaj1.prasun@ril.com>
2021-09-06 10:08:40 +0530

    Listing and Single listing.

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 def index(request):
@@ -14,7 +15,16 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    #Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'listing': listing,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
