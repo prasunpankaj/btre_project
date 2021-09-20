8f4968c3e4f2cc9e12969ed0bb5dd6268687106e
Pankaj Prasun <pankaj1.prasun@ril.com>
2021-09-20 09:24:57 +0530

    View Manage

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 # Create your views here.
 def index(request):
@@ -27,4 +28,12 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    queryset_list = Listing.objects.order_by('-list_date')
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list
+    }
+    return render(request, 'listings/search.html', context)
